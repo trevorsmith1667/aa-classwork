0c7d3759e4c31a7d1ff98ebb2e4596438e506374
Trevor Smith <trevor.smith1667@gmail.com>
2020-12-04 12:06:31 -0800

    intitialized value, parent, children, for node class

--- W3D5/Polytreenode/lib/00_tree_node.rb
+++ W3D5/Polytreenode/lib/00_tree_node.rb
@@ -1,3 +1,15 @@
 class PolyTreeNode
+    attr_reader :value, :children, :parent
+    def initialize(value, children = [])
+        @value = value
+        @parent = nil
+        @children = []
+    end 
+
+    def parent=(node)
+        # self.parent = node
+        # self.parent.children
+
+    end 
 
 end
