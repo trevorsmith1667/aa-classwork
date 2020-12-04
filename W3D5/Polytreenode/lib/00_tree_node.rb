78d4c9ecc890f263ca15c2c25890f7e098846462
Trevor Smith <trevor.smith1667@gmail.com>
2020-12-04 14:45:12 -0800

    added add, remove children

--- W3D5/Polytreenode/lib/00_tree_node.rb
+++ W3D5/Polytreenode/lib/00_tree_node.rb
@@ -19,9 +19,27 @@ class PolyTreeNode
         self
     end 
 
-    
+    def add_child(node)
+        node.parent = self 
+    end 
 
+    def remove_child(node)
+        node.parent = nil
+        raise "not child" if !self.children.include?(node)
+
+    end 
 end
+
+class Searchable 
+    def dfs(value, target)
+        
+    end 
+
+    def bfs
+    end 
+
+
+end 
 
 # n1 = PolyTreeNode.new("root1")
 # n2 = PolyTreeNode.new("root2")
