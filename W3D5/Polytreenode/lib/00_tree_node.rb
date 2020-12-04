6a385155fd83db9e3d7ff1b6b1ace15e12713a9c
numbercdli <jhwangp@gmail.com>
2020-12-04 17:23:20 -0500

    finished the parent= part

--- W3D5/Polytreenode/lib/00_tree_node.rb
+++ W3D5/Polytreenode/lib/00_tree_node.rb
@@ -1,15 +1,40 @@
+require "byebug"
+
 class PolyTreeNode
-    attr_reader :value, :children, :parent
-    def initialize(value, children = [])
+    attr_reader :value, :parent
+    def initialize(value = nil, children = [])
         @value = value
         @parent = nil
         @children = []
     end 
 
-    def parent=(node)
-        # self.parent = node
-        # self.parent.children
+    def children
+      @children
+    end
 
+    def parent=(node) # child = self
+        self.parent.children.delete(self) if self.parent
+        @parent = node
+        self.parent.children << self unless self.parent.nil?
+        self
     end 
 
+    
+
 end
+
+# n1 = PolyTreeNode.new("root1")
+# n2 = PolyTreeNode.new("root2")
+# n3 = PolyTreeNode.new("root3")
+
+# # connect n3 to n1
+# n3.parent = n1
+# # connect n3 to n2
+# n3.parent = n2
+
+# # this should work
+# raise "Bad parent=!" unless n3.parent == n2
+# raise "Bad parent=!" unless n2.children == [n3]
+
+# # this probably doesn't
+# raise "Bad parent=!" unless n1.children == []
