580d6eb8b67d025fef390543619295b00f715f79
numbercdli <jhwangp@gmail.com>
2020-12-07 13:54:34 -0500

    added somethings to kpf

--- W3D5/Polytreenode/lib/Kpf.rb
+++ W3D5/Polytreenode/lib/Kpf.rb
@@ -3,34 +3,42 @@ class KnightPathFinder
     KNIGHT_MOVES =  [[2, 1], [-2, -1], [-2, 1], [2, -1], [1, 2], [-1, -2], [-1, 2], [1, -2]]
     attr_reader :start_pos
     def initialize(start_pos)
-        @start_pos = KnightPathFinder.new([0,0]) 
-        @considered_pos = [start_pos]
+        @root_node = PolyTreeNode.new(@start_pos)
+        @considered_pos = []
+        build_move_tree(root_node)
     end 
 
     def self.build_move_tree
         self.root_node = PolyTreeNode.new(start_pos)
+
+        nodes = [root_node]
+
+        until nodes.empty?
+            new_node = nodes.shift
+            new_node_value = new_node.value
+
+            
+        end
     end 
 
     def self.valid_moves(pos)
         row, col = pos 
-        possible_moves = []
-        KNIGHT_MOVES.each do |move|  
-            if !@considered_pos.include?(pos) && KNIGHT_MOVES.include?(pos)
-                possible_moves << [row, col]
-            end 
-        end 
+
+        all_potential_moves = KNIGHT_MOVES.map do |x, y|
+            [row + x, col + y]
+        end
+
+        all_potential_moves.reject do |moves|
+            moves.any? { |x, y| x, y < 0 || x, y > 7 }
+        end
         possible_moves
     end 
 
     def new_move_positions(pos)
-        new_positions = []
-        KnightPathFinder.valid_moves(pos).each do |ele|
-            if !@considered_pos.include?(ele)
-                considered_pos << ele 
-                new_positions << ele 
-            end 
-        end 
-        new_positions
+        new_positions = KnightPathFinder.valid_moves(pos)
+        valid_pos = new_positions.reject { |position| @considered_pos.include?(position) }
+        @considered_pos.concat(valid_pos)
+        valid_pos
     end 
 
 
