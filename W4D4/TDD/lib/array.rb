62497680342b3b9185c3cdcdf23efa5bc29add7c
Trevor Smith <trevor.smith1667@gmail.com>
2020-12-10 14:40:05 -0800

    finished my_uniq specs

--- W4D4/TDD/lib/array.rb
+++ W4D4/TDD/lib/array.rb
@@ -1,6 +1,5 @@
 
 
 def my_uniq(arr)
-  p "hey, we're in here"
     arr.uniq
 end 
--- W4D4/TDD/spec/array_spec.rb
+++ W4D4/TDD/spec/array_spec.rb
@@ -1,25 +1,21 @@
 require "rspec"
 require "array"
 
-describe Array do 
+describe Array do
     let(:my_arr) {[1, 2, 1, 3, 3]}
-    describe "#my_uniq" do 
+    describe "#my_uniq" do
+        it "checks array validity" do
+            expect(my_arr).to be_a(Array)
+        end 
         it "removes duplicates" do
-          my_arr.each do |ele| 
-            expect(my_arr.my_uniq
-            expect(my_arr).to receive(my_uniq).and_return([1,2,3]) 
-                
+            expect(my_arr).to contain_exactly(1, 2, 1, 3, 3) 
+            expect(my_uniq(my_arr).uniq).to eq([1, 2, 3])
         end 
     end 
+
+    describe "#two_sum" do
+        it "when given any 2 elements whose sum amounts to 0, returns pairs in a inner array of 2D array"
+        end
+    
+    end 
 end 
-
-# describe "#[]" do
-#   it "should accept an index as an arg" do
-#     code[1]
-#   end
-
-#   it "should return the element of @pegs at the given index" do
-#     expect(code[1]).to eq("G")
-#     expect(code[2]).to eq("R")
-#   end
-# end
