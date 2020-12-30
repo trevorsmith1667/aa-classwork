571fba14148fc8a2129d1e07cfd48000ee08cf7b
colineckert <colineckert10@gmail.com>
2020-12-30 14:32:00 -0800

    Finish phase 2

--- W6D2/Art_Share_API/app/models/user.rb
+++ W6D2/Art_Share_API/app/models/user.rb
@@ -3,11 +3,13 @@ class User < ApplicationRecord
 
     has_many :artworks, 
     foreign_key: :artist_id, 
-    class_name: :Artwork
+    class_name: :Artwork,
+    dependent: :destroy
 
     has_many :shares, 
     foreign_key: :viewer_id, 
-    class_name: :Artworkshare
+    class_name: :Artworkshare, 
+    dependent: :destroy
 
     has_many :shared_artworks, 
     through: :shares, 
