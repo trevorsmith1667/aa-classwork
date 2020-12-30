73c7034112c422b336e8f23455c22171b7aa3b0b
Trevor Smith <trevor.smith1667@gmail.com>
2020-12-29 16:04:12 -0800

    added tables/models for artwork, user, artworkshare

--- W6D2/Art_Share_API/app/models/user.rb
+++ W6D2/Art_Share_API/app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
-    validates :name, :email, presence: true 
+    validates :username, presence: true, uniqueness: true
+
+    has_many :artworks, 
+    foreign_key: :artist_id, 
+    class_name: :Artwork
+
+    has_many :viewers, 
+    foreign_key: :viewer_id, 
+    class_name: :Artworkshare
+
+    has_many :shared_artworks, 
+    through: :artworks, 
+    source: :shares
 end
