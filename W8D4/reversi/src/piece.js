5822a52bc239af2956f127ff7d8eae02742731e9
Trevor Smith <trevor.smith1667@gmail.com>
2021-01-14 11:54:19 -0800

    finished oppcolor and flip

--- W8D4/reversi/src/piece.js
+++ W8D4/reversi/src/piece.js
@@ -1,19 +1,33 @@
 /**
  * Initializes the Piece with its color.
  */
+
 function Piece (color) {
+   this.color = color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+    if (this.color === "white"){
+        return "black"
+    } 
+    else{
+        return "white"
+    }
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+    if (this.color === "white"){
+        this.color ="black"
+    } 
+    else{
+        this.color = "white"
+    }
 };
 
 /**
