dd5880153836d8811cecc87716e0e82fab6cbece
Trevor Smith <trevor.smith1667@gmail.com>
2021-01-15 14:09:19 -0800

    clock stuff

--- W8D5/Callbacks/intro_callbacks.js
+++ W8D5/Callbacks/intro_callbacks.js
@@ -1,17 +1,35 @@
 class Clock {
-    constructor(){
-        const time  = function(hr, min, sec){
-            this.hr = hr
-            this.min = min
-            this.sec = sec
-
+    constructor() {
+        // 1. Create a Date object.
+        // 2. Store the hours, minutes, and seconds.
+        // 3. Call printTime.
+        // 4. Schedule the tick at 1 second intervals.
+        let current_date = new Date();
+        this.hr = current_date.getHours();
+        this.min = current_date.getMinutes();
+        this.sec = current_date.getSeconds();
+        
         }
+  
+    printTime() {
+      // Format the time in HH:MM:SS
+      // Use console.log to print it.
+      let timeString = `${this.hr} : ${this.min} : ${this.sec}`;
+      console.log(timeString)
     }
-    let current_date = new Date;
-    
-    //let current_time = new time
+  
+    _tick() {
+      // 1. Increment the time by one second.
+      // 2. Call printTime.
+    setInterval(this.printTime.bind(this), 1000)
 
-    console.log(current_date.getHours())
-    
+    // setInterval(function printTime(){
+    //     let timeString = `${this.hr} : ${this.min} : ${this.sec}`;
+    //     console.log(timeString)
+    // }, 1000)
+    }
+    }
+  
+  const clock = new Clock();
 
-}
+  clock._tick();
