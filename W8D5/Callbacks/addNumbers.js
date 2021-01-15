847bea83a7ce5580bb11524b17f5cd306e48f773
Trevor Smith <trevor.smith1667@gmail.com>
2021-01-15 15:16:28 -0800

    add nums done

--- W8D5/Callbacks/addNumbers.js
+++ W8D5/Callbacks/addNumbers.js
@@ -1,5 +1,32 @@
-const readline = require("readline");
+// const readline = require("readline");
+// const reader = readline.createInterface({
+//   input: process.stdin, 
+//   output: process.stdout
+// }) 
+
+const readline = require('readline');
+
 const reader = readline.createInterface({
-  input: process.stdin, 
+  input: process.stdin,
   output: process.stdout
-}) 
+});
+
+function addNumbers(sum, numsLeft, completionCallback){
+  if (numsLeft > 0){
+    reader.question('add number', userInput => {
+      const num = parseInt(userInput)
+      sum += num;
+    addNumbers(sum, numsLeft - 1, completionCallback);
+    
+      
+    })
+  
+  }
+  if (numsLeft === 0){
+    completionCallback(sum)
+    reader.close() 
+  } 
+}
+addNumbers(0, 3, sum => {
+  console.log(`Total Sum: ${sum}`)
+});
