038be13c47ced518573121c0d733f3c3593c9f1f
Trevor Smith <trevor.smith1667@gmail.com>
2021-01-15 12:23:06 -0800

    clocks

---
+++ W8D5/Callbacks/intro_callbacks.js
@@ -0,0 +1,17 @@
+class Clock {
+    constructor(){
+        const time  = function(hr, min, sec){
+            this.hr = hr
+            this.min = min
+            this.sec = sec
+
+        }
+    }
+    let current_date = new Date;
+    
+    //let current_time = new time
+
+    console.log(current_date.getHours())
+    
+
+}
