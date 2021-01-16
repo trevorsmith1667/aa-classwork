10fee23e63fc122bbcc5f53a3235bd0164626f5d
Trevor Smith <trevor.smith1667@gmail.com>
2021-01-15 16:18:58 -0800

    working on absurd bubblesort

--- W8D5/Callbacks/absurdBubbleSort.js
+++ W8D5/Callbacks/absurdBubbleSort.js
@@ -18,6 +18,24 @@ function askIfGreaterThan(el1, el2, callback){
   })
 }
 
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop){
+
+   if (i < arr.length - 1){
+   askIfGreaterThan(arr[i], arr[i + t], isGreaterThan => {
+     if (isGreaterThan) {
+        el1 = arr[i] 
+        el2 = arr[i + 1] 
+       arr[i + 1] = el1 
+       arr[i] = el2
+    }
+    i++ 
+
+    innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop)
+  
+
+   })
+   }
+};
 
 
 
@@ -25,6 +43,16 @@ function absurdBubbleSort(arr, sortCompletionCallback){
 
 };
 
-function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop){
+function outerBubbleSortLoop(madeAnySwaps) {
+  // Begin an inner loop if you made any swaps. Otherwise, call
+  // `sortCompletionCallback`.
+}
 
-};
+// Kick the first outer loop off, starting `madeAnySwaps` as true.
+}
+
+absurdBubbleSort([3, 2, 1], function(arr) {
+console.log("Sorted array: " + JSON.stringify(arr));
+reader.close();
+});
+
